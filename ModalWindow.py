dc60a6cef5aff01583e1a8eab129b479de64906e
Tacoo99 <koziolwojciech99@gmail.com>
2023-07-10 08:21:46 +0200

    Duplicating files working, working on directories

--- ModalWindow.py
+++ ModalWindow.py
@@ -1,31 +1,67 @@
+import tkinter
 import customtkinter
 from tkcalendar import Calendar
 
 
 class ModalWindow(customtkinter.CTkToplevel):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, callback=None, source =None, **kwargs):
         super().__init__(*args, **kwargs)
-        self.geometry("400x360")
+        
+        self.callback = callback 
+        self.calendarSource = source
+        
+        self.title("Date chooser")
+        self.geometry("310x400")
         self.resizable(False, False)
         self.iconbitmap("images/icon/icon.ico")
         
+        self.transient(self.master)
+        self.update_idletasks()
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x = (screen_width - self.winfo_width()) // 2
+        y = (screen_height - self.winfo_height()) // 2
+        self.geometry(f"+{x}+{y}")
+        self.lift()
+        self.focus_force()
+        
         self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+        
 
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.home_frame.grid_columnconfigure(0, weight=2)
+        
         self.home_frame.grid(row=0, column=1, sticky="nsew")
 
-        self.banner_text = customtkinter.CTkLabel(self.home_frame, text="SFC - Simple File Creator",
+        self.banner_text = customtkinter.CTkLabel(self.home_frame, text="Choose date",
                                                   font=customtkinter.CTkFont(size=18, weight="bold"))
         self.banner_text.grid(row=0, column=0, sticky="nsew", ipady=10)
         
         
-        self.cal = Calendar(self.home_frame, font="Arial 14", selectmode='day', locale='en_US',
-                   cursor="hand1", year=2018, month=2, day=5)
+        self.cal = Calendar(self.home_frame, font="Arial 12", selectmode='day', locale='en_US',
+                   cursor="hand2", year=2023, month=7, day=3)
         
-        self.cal.grid(row=1, column=0, sticky="nsew")
+        self.cal.bind("<<CalendarSelected>>", lambda event: self.getSelection(event, source="calendar"))
         
-        self.date = customtkinter.CTkEntry(self.home_frame, width=200, placeholder_text="Chosen date")
-        self.date.grid(row=2, column=0, pady=(15, 15), ipady=5)
+        self.cal.grid(row=1, column=0)
+        
+        self.date_entry = customtkinter.CTkEntry(self.home_frame, width=200, placeholder_text="Chosen date")
+        self.date_entry.grid(row=2, column=0, pady=(15, 15), ipady=5)
+        
+        self.confirm_button= customtkinter.CTkButton(self.home_frame, text="Confirm", command=lambda: self.getSelection(None, source="button"))
+        self.confirm_button.grid(row=3, column=0, pady=(15, 15), ipady=5)
+        
+    def getSelection(self, event, source):
+        selected_date = self.cal.selection_get()
+        if source == "calendar":
+            self.date_entry.delete(0, 'end')
+            self.date_entry.insert(0, selected_date)
+        elif source == "button":
+            if self.date_entry.get() == '':
+                tkinter.messagebox.showerror(title='Error', message='Please choose a date!')
+            else:
+                if self.callback:
+                    self.callback(selected_date, self.calendarSource)
+                self.destroy()
+        
         
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import tkinter
 import customtkinter
 import os
 from PIL import Image
 from tkinter import filedialog as fd
-
+from pygame import mixer
+from datetime import datetime, timedelta
+import shutil
 
 
 from ModalWindow import ModalWindow
@@ -14,50 +17,43 @@ class App(customtkinter.CTk):
         self.toplevel_window = None
 
         self.title("SFC")
-        self.geometry("400x300")
+        self.geometry("400x690")
         self.resizable(False, False)
         self.iconbitmap("images/icon/icon.ico")
         
+        self.update_idletasks()
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x = (screen_width - self.winfo_width()) // 2
+        y = (screen_height - self.winfo_height()) // 2
+        self.geometry(f"+{x}+{y}")
+        
 
         self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        
+        music_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "music")
+        mixer.init()
+        mixer.music.load(os.path.join(music_path, "metinek.mp3"))
+        mixer.music.play(-1)
+        
+        self.fullPath = ''
+        self.check_var = customtkinter.StringVar(value="file")
+        
 
         # load images with light and dark mode image
         image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
-        # self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "banner-logo.png")), size=(110, 51))
         
-<<<<<<< HEAD
-        self.file_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "file_dark.png")),
-                                                 dark_image=Image.open(os.path.join(image_path, "file_light.png")), size=(20, 20))
-=======
-        self.first_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "first_dark.png")),
-                                                     dark_image=Image.open(os.path.join(image_path, "first_light.png")), size=(25, 25))
-        self.second_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "second_dark.png")),
-                                                     dark_image=Image.open(os.path.join(image_path, "second_light.png")), size=(25, 25))
-        self.third_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "third_dark.png")),
-                                                     dark_image=Image.open(os.path.join(image_path, "third_light.png")), size=(25, 25))
-
-        # create navigation frame
-        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
-        self.navigation_frame.grid(row=0, column=0, sticky="nsew" )
-        self.navigation_frame.grid_rowconfigure(3, weight=1)
-
-        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="", image=self.logo_image)
-        self.navigation_frame_label.grid(row=0, column=0, padx=0, pady=25, sticky="nsew")
-
-        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Add account",
-                                                   fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                   image=self.add_user_image, anchor="w", command=self.home_button_event)
-        self.home_button.grid(row=1, column=0, sticky="ew")
-
-        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Info/FAQ",
-                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.info_image, anchor="w", command=self.frame_2_button_event)
-        self.frame_2_button.grid(row=2, column=0, sticky="ew")
->>>>>>> 6b207a8d48361ace57a1eaef93cfa13c70686da2
         
-        self.date_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "date_dark.png")),
-                                                 dark_image=Image.open(os.path.join(image_path, "date_light.png")), size=(20, 20))
+        self.file_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "file_light.png")), size=(20, 20))
+        self.date_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "date_light.png")), size=(20, 20))
+        self.folder_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "folder_light.png")), size=(20, 20))
+        self.generate_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "generate_light.png")), size=(20, 20))
+        self.speaker_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "speaker_dark.png")),
+                                                     dark_image=Image.open(os.path.join(image_path, "speaker_light.png")), size=(20, 20))
+        self.mute_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "mute_dark.png")),
+                                                     dark_image=Image.open(os.path.join(image_path, "mute_light.png")), size=(20, 20))
+        
 
         # create home frame
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
@@ -70,91 +66,142 @@ class App(customtkinter.CTk):
                                                   font=customtkinter.CTkFont(size=14))
         self.description_text.grid(row=1, column=0, padx=35, sticky="n")
 
+         #file or directory checkbox
+        self.fileOrDir_checkbox = customtkinter.CTkCheckBox(self.home_frame, text=f'Chosen: file', command=self.get_file_or_dir,
+                                     variable=self.check_var, onvalue="file", offvalue="directory" state="disabled")
+        self.fileOrDir_checkbox.grid(row=4, column=0, padx=35, pady=10, sticky="n")
+        
         #file chooser
         self.filename_entry = customtkinter.CTkEntry(self.home_frame, width=200, placeholder_text="Filename")
         self.filename_entry.grid(row=2, column=0, pady=(15, 15), ipady=5)
         
+        
+         #file or directory chooser button
         self.button_fileChooser = customtkinter.CTkButton(self.home_frame, text="Choose file", command=self.select_file, image=self.file_image, compound="right")
         self.button_fileChooser.grid(row=3, column=0, ipady=5)
         
         
-<<<<<<< HEAD
-        self.password_entry = customtkinter.CTkEntry(self.home_frame, width=200, placeholder_text="Date")
-        self.password_entry.grid(row=4, column=0, padx=30, pady=(15, 15), ipady=5)
+        #directory chooser
+        self.directory_entry = customtkinter.CTkEntry(self.home_frame, width=200, placeholder_text="Directory")
+        self.directory_entry.grid(row=5, column=0, pady=(15, 15), ipady=5)
         
-        self.home_frame_button_2 = customtkinter.CTkButton(self.home_frame, text="Choose date", command=self.open_toplevel, image=self.date_image, compound="right")
-        self.home_frame_button_2.grid(row=5, column=0, ipady=5)
+        self.button_directoryChooser = customtkinter.CTkButton(self.home_frame, text="Choose destination", command=self.select_directory, image=self.folder_image, compound="right")
+        self.button_directoryChooser.grid(row=6, column=0, ipady=5)
+        
+        
+         #starting date
+        self.startingDate_entry = customtkinter.CTkEntry(self.home_frame, width=200, placeholder_text="Starting date")
+        self.startingDate_entry.grid(row=7, column=0, padx=30, pady=(15, 15), ipady=5)
+        
+        self.startingDate_button = customtkinter.CTkButton(self.home_frame, text="Choose date", command=self.starting_date, image=self.date_image, compound="right")
+        self.startingDate_button.grid(row=8, column=0, ipady=5)
 
-        self.home_frame.grid(row=0, column=1, sticky="nsew")
+         #ending date
+        self.endingDate_entry = customtkinter.CTkEntry(self.home_frame, width=200, placeholder_text="Ending date")
+        self.endingDate_entry.grid(row=9, column=0, padx=30, pady=(15, 15), ipady=5)
         
-=======
-        self.banner_text2 = customtkinter.CTkLabel(self.help_frame, text="Frequently asked questions",
-                                                  font=customtkinter.CTkFont(size=18, weight="bold"))
-        self.banner_text2.grid(row=0, column=0, padx=20, pady=0, sticky="nsew", ipady=10)
-        self.description_text2 = customtkinter.CTkLabel(self.help_frame, text="Find answers to commonsly asked questions about this program",
-                                                  font=customtkinter.CTkFont(size=14))
-        self.description_text2.grid(row=1, column=0, padx=20, sticky="n")
+        self.endingDate_button= customtkinter.CTkButton(self.home_frame, text="Choose date", command=self.ending_date, image=self.date_image, compound="right")
+        self.endingDate_button.grid(row=10, column=0, ipady=5)
         
-        #first question
-        self.first_title = customtkinter.CTkLabel(self.help_frame, text="What is this program for?", image=self.first_image, compound="left")
-        self.first_title.grid(row=2, column=0, ipadx=50, pady=25, sticky="wn")
+         #generateFiles button
+        self.generateFiles_button= customtkinter.CTkButton(self.home_frame, text="Generate files", command=self.generate_files, image=self.generate_image, compound="right")
+        self.generateFiles_button.grid(row=11, column=0, padx=30, pady=(30, 15), ipady=5, sticky='nsew')
         
-        self.first_description = customtkinter.CTkLabel(self.help_frame, text="It allows you to log in to your domain account \nwhen you receive new computer")
-        self.first_description.grid(row=3, column=0, sticky="n")
+        self.speaker_button = customtkinter.CTkButton(self.home_frame, text="", width=200, image=self.speaker_image, fg_color='transparent', hover=False, compound="left", command=self.play_music)
+        self.speaker_button.grid(row=12, column=0, padx=0, sticky="w")
         
-        #second question
-        self.second_title = customtkinter.CTkLabel(self.help_frame, text="Who to contact in case of problem?", image=self.second_image, compound="left")
-        self.second_title.grid(row=4, column=0, ipadx=50, pady=25, sticky="wn")
-        
-        self.second_description = customtkinter.CTkLabel(self.help_frame, text="E-mail: wojciech.koziol@fujitsu.com\nE-mail: servicedesk@mpl.mee.com\nTelephone: +48 22 104 32 61")
-        self.second_description.grid(row=5, column=0, sticky="n")
-        
-        #third question
-        self.third_title = customtkinter.CTkLabel(self.help_frame, text="Who is the author of the program?", image=self.third_image, compound="left")
-        self.third_title.grid(row=6, column=0, ipadx=50, pady=25, sticky="wn")
-        
-        self.third_description = customtkinter.CTkLabel(self.help_frame, text="Wojciech Kozioł, Fujitsu")
-        self.third_description.grid(row=7, column=0, sticky="n")
-        
-        self.third_description = customtkinter.CTkLabel(self.help_frame, text="CUF ©2023 Fujitsu. All Rights Reserved",
-                                                        font=customtkinter.CTkFont(size=9))
-        self.third_description.grid(row=8, column=0, pady=15,padx=5, sticky="se")
-        
-
-        # select default frame
-        self.select_frame_by_name("home")
-
-    def select_frame_by_name(self, name):
-        # set button color for selected button
-        self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
-        self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
-
-        # show selected frame
-        if name == "home":
-            self.home_frame.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.home_frame.grid_forget()
-        if name == "frame_2":
-            self.help_frame.grid(row=0, column=1, sticky="nsew")
-        else:
-            self.help_frame.grid_forget()
-
-    def home_button_event(self):
-        self.select_frame_by_name("home")
-
-    def frame_2_button_event(self):
-        self.select_frame_by_name("frame_2")
-
-    def change_appearance_mode_event(self, new_appearance_mode):
-        customtkinter.set_appearance_mode(new_appearance_mode)
->>>>>>> 6b207a8d48361ace57a1eaef93cfa13c70686da2
-        
-    def open_toplevel(self):
-        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
-            self.toplevel_window = ModalWindow(self)  # create window if its None or destroyed
-        else:
-            self.toplevel_window.focus()  # if window exists focus it
+        self.slider_1 = customtkinter.CTkSlider(self.home_frame, from_=0.0, to=1.0, command=self.slider_event)
+        self.slider_1.grid(row=13, column=0, sticky="w")
     
+        self.home_frame.grid(row=0, column=1)
+        
+    def set_full_path(self, path):
+        self.fullPath = path
+    
+    def get_full_path(self):
+        return self.fullPath
+    
+    def get_file_or_dir(self):
+        fileOrDir = self.check_var.get()
+        self.fileOrDir_checkbox.configure(text=f'Chosen: {fileOrDir}')
+        
+        if fileOrDir == 'directory':
+            self.button_fileChooser.configure(image=self.folder_image)
+            self.filename_entry.configure(placeholder_text="Directory")
+            self.button_fileChooser.configure(text="Choose folder")
+        if fileOrDir == 'file':
+            self.button_fileChooser.configure(image=self.file_image)
+            self.filename_entry.configure(placeholder_text="Filename")
+            self.button_fileChooser.configure(text="Choose file")
+        
+        
+    def slider_event(self, value):
+        mixer.music.set_volume(value)
+        
+    def play_music(self):
+        if mixer.music.get_volume() == 0.0:
+            mixer.music.set_volume(1.0)
+            self.speaker_button.configure(image=self.speaker_image)
+           
+        else:
+            mixer.music.set_volume(0.0)
+            self.speaker_button.configure(image=self.mute_image)
+            
+    def duplicate_file_or_directory_with_dates(self,filename, directory, date_from, date_to):
+        num_copies = 0
+        date_format = "%Y-%m-%d"
+        date_start = datetime.strptime(date_from, date_format).date()
+        date_end = datetime.strptime(date_to, date_format).date()
+
+        delta = date_end - date_start
+        num_days = delta.days
+
+        for i in range(num_days + 1):
+            current_date = date_start + timedelta(days=i)
+            date_str = current_date.strftime(date_format)
+            source_path = filename
+            destination_path = os.path.join(directory, f"{date_str}_{os.path.basename(filename)}")
+
+            if os.path.isdir(filename):
+                shutil.copytree(source_path, destination_path)
+                num_copies += 1
+            elif os.path.isfile(filename):
+                shutil.copy2(source_path, destination_path)
+                num_copies += 1
+            else:
+                tkinter.messagebox.showerror(title='Error', message='Wrong file or directory, choose another one!')
+        tkinter.messagebox.showinfo(title='Success', message=f"Copied {num_copies} of files or directories")
+        
+    def generate_files(self):
+        
+        if self.filename_entry.get() != '' and self.directory_entry.get() != '' and self.startingDate_entry.get() != '' and self.endingDate_entry.get() != '':
+            self.duplicate_file_or_directory_with_dates(self.get_full_path(), self.directory_entry.get(), self.startingDate_entry.get(), self.endingDate_entry.get())
+        else:
+            tkinter.messagebox.showerror("Error!", "Please fill in all fields!")
+        
+    def starting_date(self):
+        source = 'start'
+        self.open_toplevel(source)
+        
+    def ending_date(self):
+        source = 'end'
+        self.open_toplevel(source)
+        
+        
+    def open_toplevel(self, buttonSource):
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window = ModalWindow(self, callback=self.handleSelectedDate, source = buttonSource  )
+        else:
+            self.toplevel_window.focus()
+    
+    def select_directory(self):
+        filename = fd.askdirectory(
+        title='Select directory',
+        )
+        
+        self.directory_entry.delete(0, 'end')
+        self.directory_entry.insert(0, filename)
+        
     def select_file(self):
         filetypes = (
         ('Excel files', '*.xlsx'),
@@ -165,9 +212,24 @@ class App(customtkinter.CTk):
         title='Open a file',
         initialdir='/',
         filetypes=filetypes)
-
-        self.filename_entry.insert(0, filename)
+        self.set_full_path(filename)
+        basename = os.path.basename(filename)
+        
+        self.filename_entry.delete(0, 'end')
+        self.filename_entry.insert(0, basename)
     
+    def handleSelectedDate(self, selected_date, source):
+        
+        if source == 'start':
+            self.startingDate_entry.delete(0, 'end')
+            self.startingDate_entry.insert(0, selected_date)
+            
+        elif source == 'end':
+            self.endingDate_entry.delete(0, 'end')
+            self.endingDate_entry.insert(0, selected_date)
+        else:
+            tkinter.messagebox.showerror(title='Error', message='You have chosen wrong date!')
+        
 
 
 if __name__ == "__main__":
